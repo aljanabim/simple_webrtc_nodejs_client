cf63b065fe94f289306e8f7ff20e213835d17ee4
aljanabim <ma970101@yahoo.se>
2021-05-22 22:40:11 +0200

    CLEARER docs and renamed hosturi

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const WebrtcManager = require("./lib/webrtc-manager");
 const dataChannelHandler = require("./lib/webrtc-handlers/data-channel-handler");
 // CONSTANTS
 const TOKEN = process.env.TOKEN;
-const HOST_URL = process.env.SIGNALING_SERVER_URI;
+const SIGNALING_SERVER_URL = process.env.SIGNALING_SERVER_URL;
 const PEER_ID = argv.id ? argv.id : os.hostname().replace(/[^a-zA-Z]/g, ""); // get hostname and strip it from special characters
-/** @type {string} - can be 'admin' | 'vehicle' | 'cc' */
+/** @type {string} - can for example be 'admin' | 'vehicle' | 'robot'  depending on you application*/
 const PEER_TYPE = "admin";
 
-// SETUP SIGNALING CHANNEL
-const channel = new SignalingChannel(PEER_ID, PEER_TYPE, HOST_URL, TOKEN);
+// SETUP SIGNALING CHANNEL AND WEBRTC
+const channel = new SignalingChannel(PEER_ID, PEER_TYPE, SIGNALING_SERVER_URL, TOKEN);
 const webrtcOptions = { enableDataChannel: true, enableStreams: false, dataChannelHandler };
 const manager = new WebrtcManager(PEER_ID, PEER_TYPE, channel, webrtcOptions);
 channel.connect();
+
+/*
+    YOUR CODE HERE - In this file you can right the overall logic of your application
+    once a succesfull peer-to-peer connetion is established.
+*/
